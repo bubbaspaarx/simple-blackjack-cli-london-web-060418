02d037d9c0265f10952f4807dc8ffab2e51cb22c
Edward Sparks <ed.sparks@flatironschool.com>
2018-08-17 16:26:25 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,10 +30,9 @@ end
 
 def initial_round
   # code #initial_round here
-  card1 = deal_card
-  card2 = deal_card
-  total = card1 + card2
+  total = deal_card + deal_card
   display_card_total(total)
+  total
 end
 
 def hit?(current_total)
